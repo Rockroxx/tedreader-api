9a044479d19237564c87cdfab88228e4680bb59a
gert <gspoelstra@gmail.com>
2017-08-21 15:36:14 +0100

    Convert and remove on each package instead of extracting all then converting all to save space.

--- app/Console/Commands/ConvertPackageToJson.php
+++ app/Console/Commands/ConvertPackageToJson.php
@@ -20,37 +20,18 @@ class ConvertPackageToJson extends Command{
             $list = app('files')->files('storage/app/ted/raw');
         }
 
-        $this->info("Extracting packages");
+        $this->info("Extracting and converting packages");
+        $this->extractAndRemoveTar($list);
+
+        // We do this twice in case there are tars inside tars.
+        $list = app('files')->files('storage/app/ted/raw');
         $this->extractAndRemoveTar($list);
 
         $this->line("Finished extracting. Starting conversion into json.");
 
-            $folders = app('files')->directories('storage/app/ted/raw/');
-            foreach($folders as $folder){
-                $folderName = explode('/', $folder);
-                $folderName = array_pop($folderName);
-                $this->line("Converting $folderName.");
-                $files = app('files')->files($folder);
-                foreach($files as $file){
-                    if(strpos($file, '.xml') == false)
-                        continue;
-                    $fileName = explode('/', $file);
-                    $fileName = explode('.', array_pop($fileName))[0];
-                    $x = new Extractor($file);
-                    $parsed = Parser::handle($x, Identify::base($x));
-                    app('files')->put("storage/app/ted/raw/$folderName/$fileName.json", json_encode($parsed));
-                    unlink($file);
-                }
-                exec("cd ".storage_path('app/ted/raw')." && tar -czf $folderName-json.tar.gz $folderName", $output);
-                if($output){
-                    throw new \Exception("Error creating json tar $folderName with the following error messages.\n\n".implode("\n", $output));
-                }
-                array_map('unlink', glob("$folder/*.*"));
-                rmdir($folder);
-            }
     }
 
-    private function extractAndRemoveTar($list, $extracted = false){
+    private function extractAndRemoveTar($list){
         foreach($list as $tar){
             if(strpos($tar, '.tar.gz') > 0 && strpos($tar, '-json.tar.gz') == false){
                 $folderName = explode('/', $tar);
@@ -60,16 +41,36 @@ class ConvertPackageToJson extends Command{
                 if($output){
                     throw new \Exception("Error extracting $tar with the following error messages.\n\n".implode("\n", $output));
                 }
-
-                $extracted = true;
                 unlink($tar);
+                $this->convert();
             }
         }
 
-        if($extracted == false){
-            // in case the tar.gz contains more tar.gz such as those before early 2012 we extract again
-            $list = app('files')->files('storage/app/ted/raw');
-            $this->extractAndRemoveTar($list, true);
+    }
+
+    private function convert(){
+        $folders = app('files')->directories('storage/app/ted/raw/');
+        foreach($folders as $folder){
+            $folderName = explode('/', $folder);
+            $folderName = array_pop($folderName);
+            $this->line("Converting $folderName.");
+            $files = app('files')->files($folder);
+            foreach($files as $file){
+                if(strpos($file, '.xml') == false)
+                    continue;
+                $fileName = explode('/', $file);
+                $fileName = explode('.', array_pop($fileName))[0];
+                $x = new Extractor($file);
+                $parsed = Parser::handle($x, Identify::base($x));
+                app('files')->put("storage/app/ted/raw/$folderName/$fileName.json", json_encode($parsed));
+                unlink($file);
+            }
+            exec("cd ".storage_path('app/ted/raw')." && tar -czf $folderName-json.tar.gz $folderName", $output);
+            if($output){
+                throw new \Exception("Error creating json tar $folderName with the following error messages.\n\n".implode("\n", $output));
+            }
+            array_map('unlink', glob("$folder/*.*"));
+            rmdir($folder);
         }
     }
 
