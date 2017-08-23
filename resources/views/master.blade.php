42fb7ca9fc207d583e0c8a95935ab1afa478c8ef
gert <gspoelstra@gmail.com>
2017-08-23 11:52:18 +0100

    Optimizing css loading

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,12 +4,11 @@
     <meta charset="utf-8">
     <title>TED Reader</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div id="app"></div>
 <div style="clear:both"></div>
-<script>
+<script type="text/javascript">
     window.api_url = "https://api.tedreader.co.uk/";
     @if(isset($total))
         {!! "window.total = ".$total !!};
@@ -37,6 +36,22 @@
     @endif
 
 </script>
-<script src="/dist/build.js"></script>
+<noscript id="deferred-styles">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+</noscript>
+<script>
+    var loadDeferredStyles = function() {
+        var addStylesNode = document.getElementById("deferred-styles");
+        var replacement = document.createElement("div");
+        replacement.innerHTML = addStylesNode.textContent;
+        document.body.appendChild(replacement)
+        addStylesNode.parentElement.removeChild(addStylesNode);
+    };
+    var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+        webkitRequestAnimationFrame || msRequestAnimationFrame;
+    if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+    else window.addEventListener('load', loadDeferredStyles);
+</script>
+<script src="/dist/build.js" ></script>
 </body>
 </html>
